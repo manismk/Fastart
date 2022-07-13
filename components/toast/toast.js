5524a619bc35040c0b5c1c9d82696bcdf0fecf2c
manismk <61050821+manismk@users.noreply.github.com>
2022-07-13 07:57:22 +0530

    fix: fab css and toast js added

--- components/toast/toast.js
+++ components/toast/toast.js
@@ -2,6 +2,10 @@ const btnTopRight = document.querySelector("#topRight");
 const btnBottomLeft = document.querySelector("#bottomLeft");
 const btnBottomCenter = document.querySelector("#bottomCenter");
 
+const btnCloseTopRight = document.querySelector("#closeTopRight");
+const btnCloseBottomLeft = document.querySelector("#closeBottomLeft");
+const btnCloseBottomCenter = document.querySelector("#closeBottomCenter");
+
 const toastTopRight = document.querySelector(".toast--top--right");
 const toastBottomLeft = document.querySelector(".toast--bottom--left");
 const toastBottomCenter = document.querySelector(".toast--bottom--center");
@@ -18,11 +22,25 @@ btnBottomCenter.addEventListener("click", () => {
   showToast(toastBottomCenter);
 });
 
+btnCloseTopRight.addEventListener("click", () => {
+  closeToast(toastTopRight);
+});
+btnCloseBottomCenter.addEventListener("click", () => {
+  closeToast(toastBottomCenter);
+});
+btnCloseBottomLeft.addEventListener("click", () => {
+  closeToast(toastBottomLeft);
+});
+
 const showToast = (toastContainer) => {
   toastContainer.classList.toggle("hide");
   setTimeout(() => {
-    if (!toastContainer.classList.contains("hide")) {
-      toastContainer.classList.toggle("hide");
-    }
+    closeToast(toastContainer);
   }, 5000);
+};
+
+const closeToast = (toastContainer) => {
+  if (!toastContainer.classList.contains("hide")) {
+    toastContainer.classList.toggle("hide");
+  }
 };
